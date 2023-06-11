64fa9fc6a529ff8852b24080bad6b783dd7b6b09
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-06-11 17:34:13 -0400

    Server connected

--- api/project/router.js
+++ api/project/router.js
@@ -1,7 +1,7 @@
-// build your `/api/projects` router here
+
 const router = require('express').Router()
 
-router.use('*', (req, res, next) => {
+router.use('*', (req, res) => {
     res.json({api: 'up'})
 })
 
@@ -9,7 +9,7 @@ router.use((err, req, res, next) => {
     res.status(500).json({
         customMessage: ' Something went wrong in the project router',
         message: err.message,
-        stack: err.stack.
+        stack: err.stack,
     })
 })
 
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,17 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+
+router.use('*', (req, res, ) => {
+    res.json({api: 'up'})
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: ' Something went wrong in the resource router',
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+
+module.exports = router
