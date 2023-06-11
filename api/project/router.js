0b5bfeebc43cfcb8ec8376a24d20494ccf14b014
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-06-11 17:26:22 -0400

    Completed Json commands

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,17 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+
+router.use('*', (req, res, next) => {
+    res.json({api: 'up'})
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: ' Something went wrong in the project router',
+        message: err.message,
+        stack: err.stack.
+    })
+})
+
+
+module.exports = router
