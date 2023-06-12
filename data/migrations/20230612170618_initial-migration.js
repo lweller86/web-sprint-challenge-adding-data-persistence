642b403451fa6275589ccf03ebe54c879da225c2
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-06-12 14:11:04 -0400

    Completed Building tables

--- data/migrations/20230612170618_initial-migration.js
+++ data/migrations/20230612170618_initial-migration.js
@@ -7,18 +7,46 @@ exports.up = async function (knex) {
         .createTable('project', table => {
             table.increments('project_id')
             table.string('project_name', 200).notNullable().unique()
+            table.string('project_description', 200)
+            table.integer('project_completed').defaultTo(0)
         })
         .createTable('resources', table => {
             table.increments('resources_id')
             table.string('resources_name', 200).notNullable().unique()
+            table.string('resources_description', 200)
         })
         .createTable('task', table => {
             table.increments('task_id')
-            table.string('task_name', 200).notNullable().unique()
+            table.string('task_description', 200).notNullable()
+            table.string('task_notes', 200)
+            table.integer('task_completed').defaultTo(0)
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('project')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+
         })
         .createTable('project_resources', table => {
             table.increments('project_resources_id')
-            table.string('project_resources_name', 200).notNullable().unique()
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('project')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            table.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('resource_id')
+                .inTable('resource')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+
+
         })
 };
 
